e6ae064e18cb3826a85af4e23f942040863b9be1
zengzhiwen <zhiwen.zeng@gmail.com>
2016-04-18 19:52:10 +0800

    add local repo

---
+++ src/main/java/MethodTest.java
@@ -0,0 +1,11 @@
+package com.github.zzwwws.sublibrary;
+
+/**
+ * Created by zzwwws on 2016/4/18.
+ */
+public class MethodTest {
+
+    public static void A(){
+        System.out.println("MethodTest===========>A");
+    }
+}
